06e5b99708548b0505292e1b1aadef82988a629e
Daniel Tremain <tremainlabs@gmail.com>
2026-05-28 11:53:48 -0700

    Added a decrease and reset button to the count component.

--- src/count.jsx
+++ src/count.jsx
@@ -5,7 +5,9 @@ function Counter(){
         return(
         <>
              <p>Count: {count}</p>
-             <button onClick={() => setCount(count + 1)}>Click me to Add one</button>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+            <button onClick={() => setCount(count - 1)}>Decrease</button>
+            <button onClick={() => setCount(0)}>Reset</button>
         </>
         )
     
