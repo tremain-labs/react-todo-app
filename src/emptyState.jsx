d4ee44b9670cb4f4e7d3d36a6c785a91c98f7fb4
Daniel Tremain <tremainlabs@gmail.com>
2026-05-29 10:32:00 -0700

    Added an empty state component.

---
+++ src/emptyState.jsx
@@ -0,0 +1,14 @@
+
+
+function EmptyState() {
+    return (
+        <>
+            <div className="empty-state">
+                <h2>No tasks at this time.</h2>
+                <p>Try adding some tasks!</p>
+
+            </div>
+        </>
+    )
+}
+export default EmptyState
